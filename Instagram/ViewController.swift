9612290be889674b593224c6ad85d37b23a8a70f
yuu <yxyz0926@gmail.commkdir ~/.sshtouch ~/.ssh/configchmod 700 ~/.sshchmod 600 ~/.ssh/*xattr -cr ~/.ssh>
2021-01-17 21:53:59 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Yu iwawaki on 2021/01/17.
+//  Copyright © 2021 Yu.iwawaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
