e21901b7bb0436a0bcefe3c2b0fe691e63732f8f
yuu <yxyz0926@gmail.commkdir ~/.sshtouch ~/.ssh/configchmod 700 ~/.sshchmod 600 ~/.ssh/*xattr -cr ~/.ssh>
2021-01-23 13:00:13 +0900

    （コメント）outletとActionをして、投稿ボタンを押すとHOME画面へ戻るようにした。

--- Instagram/CommentViewController.swift
+++ Instagram/CommentViewController.swift
@@ -11,16 +11,32 @@ import UIKit
 class CommentViewController: UIViewController{
 
     
+    @IBOutlet weak var commentTextField: UITextField!
     
     
     
    
+    @IBAction func commentButton(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+         
         
     }
     
